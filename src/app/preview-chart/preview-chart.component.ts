ab3aa719291b05beb9c5d72a5556f9a4686ffda3
Tobias Enthaler <tobias@enthaler.dev>
2024-10-23 18:08:26 +0200

    Add chart.js and create demo chart based on dynamic columns

--- src/app/preview-chart/preview-chart.component.ts
+++ src/app/preview-chart/preview-chart.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { DataproviderService } from '../dataprovider.service';
+import { DataPoint } from '../../interfaces/dataPoint.interface';
 
 @Component({
   selector: 'luis-preview-chart',
   standalone: true,
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './preview-chart.component.html',
   styleUrl: './preview-chart.component.scss'
 })
-export class PreviewChartComponent {
+export class PreviewChartComponent implements OnInit {
+  data: DataPoint[] = [];
+
+  constructor(private dataProvider: DataproviderService) {}
+
+  ngOnInit(): void {
+    this.dataProvider.getDemoDataHTML().subscribe(r => {
+      this.data = r;
+    });
+  }
+
+  generateChartData() {
+    var d = {
+      labels: this.data.map(x => x.timestamp.toLocaleDateString(undefined, { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric'})),
+      datasets: <any>[]
+    };
+
+    Object.keys(this.data?.[0] ?? {}).slice(1).forEach(key => {
+      d.datasets.push({
+        label: key,
+        data: this.data.map(x => x[key]),
+        fill: false
+      });
+    });
+
+    return d;
+  }
 
 }
