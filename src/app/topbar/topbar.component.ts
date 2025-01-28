4ad853b4ef07a281c858648c9ecf916a59f641c4
Jakob Hanner <jakob.hanner@edu.fh-joanneum.at>
2025-01-28 17:35:48 +0100

    Add Final Logo and Favicon

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'luis-topbar',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageModule
+  ],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
-
+  logoPath : string;
+  constructor() {
+    this.logoPath = 'assets/logo/LuisV2Logo.png';
+  }
 }
